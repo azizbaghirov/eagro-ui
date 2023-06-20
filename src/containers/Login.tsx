8c74ea3ae1dc197e7268142dac6e263a646fba4b
azizbaghirov <104507495+azizbaghirov@users.noreply.github.com>
2023-06-20 11:05:27 +0400

    Update Login.tsx

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -94,7 +94,7 @@ export const Login = () => {
         </div>
         <div className="mb-20 text-center">
           <span className="text-grey-300 text-lg lg:text-2xl">
-            Elektron Kənd Təsərrüfatı İnformasiya Sistemiii
+            Elektron KKKənd Təsərrüfatı İnformasiya Sistem
           </span>
         </div>
         {/* <div className="mb-11 text-center">
